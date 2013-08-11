e0a8655bb7a4dee3b5d3479931379d2588e9175f
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-08-11 17:21:55 +0900

    Add base functions for reading Gyro value.

--- demos/ARMCM4-STM32F303-DISCOVERY/main.c
+++ demos/ARMCM4-STM32F303-DISCOVERY/main.c
@@ -61,6 +61,63 @@ static msg_t Thread1(void *arg) {
 #endif /* 0 */
 
 
+static const SPIConfig spi1cfg = {
+  NULL,
+  /* HW dependent part.*/
+  GPIOE,
+  GPIOE_SPI1_CS,
+  SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_CPOL | SPI_CR1_CPHA,
+  0
+};
+
+uint8_t readByteSPI(uint8_t reg)
+{
+	char txbuf[2] = {0x80 | reg, 0xFF};
+	char rxbuf[2];
+	spiSelect(&SPID1);
+	spiExchange(&SPID1, 2, txbuf, rxbuf);
+	spiUnselect(&SPID1);
+	return rxbuf[1];
+}
+
+uint8_t writeByteSPI(uint8_t reg, uint8_t val)
+{
+	char txbuf[2] = {reg, val};
+	char rxbuf[2];
+	spiSelect(&SPID1);
+	spiExchange(&SPID1, 2, txbuf, rxbuf);
+	spiUnselect(&SPID1);
+	return rxbuf[1];
+}
+
+void initGyro(void)
+{
+    /* see the L3GD20 Datasheet */
+    writeByteSPI(0x20, 0xcF);
+}
+
+int readGyro(float *data_x, float *data_y, float *data_z)
+{
+    /* read from L3GD20 registers and assemble data */
+    /* 0xc0 sets read and address increment */
+    char txbuf[8] = {0xc0 | 0x27, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    char rxbuf[8];
+    spiSelect(&SPID1);
+    spiExchange(&SPID1, 8, txbuf, rxbuf);
+    spiUnselect(&SPID1);
+    if (rxbuf[1] & 0x7) {
+        int16_t val_x = (rxbuf[3] << 8) | rxbuf[2];
+        int16_t val_y = (rxbuf[5] << 8) | rxbuf[4];
+        int16_t val_z = (rxbuf[7] << 8) | rxbuf[6];
+#define MDPS_PER_DIGIT (8.75)
+        *data_x = (((float)val_x) * MDPS_PER_DIGIT)/1000.0;
+        *data_y = (((float)val_y) * MDPS_PER_DIGIT)/1000.0;
+        *data_z = (((float)val_z) * MDPS_PER_DIGIT)/1000.0;
+        return 1;
+    }
+    return 0;
+}
+
 /*
  * Application entry point.
  */
@@ -82,6 +139,9 @@ int main(void) {
   sdStart(&SD1, NULL);
   palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));
   palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));
+
+  spiStart(&SPID1, &spi1cfg);
+  initGyro();
 
 #if 0
   /*
