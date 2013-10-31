899836cc213b90a2f29b744927ca226007798b99
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-10-31 16:18:07 +0900

    Put Haskell application on demos/*_hs/ dir.

--- demos/ARMCM4-STM32F303-DISCOVERY_hs/main.c
+++ demos/ARMCM4-STM32F303-DISCOVERY_hs/main.c
@@ -17,7 +17,12 @@
 #include "ch.h"
 #include "hal.h"
 #include "test.h"
+#include "jhc_rts_header.h"
 
+extern void malloc_init(void);
+extern void forkOS_createThread_init(void);
+
+#if 0
 /*
  * This is a periodic thread that does absolutely nothing except flashing LEDs.
  */
@@ -53,12 +58,70 @@ static msg_t Thread1(void *arg) {
     palClearPad(GPIOE, GPIOE_LED4_BLUE);
   }
 }
+#endif /* 0 */
+
+
+static const SPIConfig spi1cfg = {
+  NULL,
+  /* HW dependent part.*/
+  GPIOE,
+  GPIOE_SPI1_CS,
+  SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_CPOL | SPI_CR1_CPHA,
+  0
+};
+
+uint8_t readByteSPI(uint8_t reg)
+{
+	char txbuf[2] = {0x80 | reg, 0xFF};
+	char rxbuf[2];
+	spiSelect(&SPID1);
+	spiExchange(&SPID1, 2, txbuf, rxbuf);
+	spiUnselect(&SPID1);
+	return rxbuf[1];
+}
+
+uint8_t writeByteSPI(uint8_t reg, uint8_t val)
+{
+	char txbuf[2] = {reg, val};
+	char rxbuf[2];
+	spiSelect(&SPID1);
+	spiExchange(&SPID1, 2, txbuf, rxbuf);
+	spiUnselect(&SPID1);
+	return rxbuf[1];
+}
+
+void initGyro(void)
+{
+    /* see the L3GD20 Datasheet */
+    writeByteSPI(0x20, 0xcF);
+}
+
+int readGyro(float *data_x, float *data_y, float *data_z)
+{
+    /* read from L3GD20 registers and assemble data */
+    /* 0xc0 sets read and address increment */
+    char txbuf[8] = {0xc0 | 0x27, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    char rxbuf[8];
+    spiSelect(&SPID1);
+    spiExchange(&SPID1, 8, txbuf, rxbuf);
+    spiUnselect(&SPID1);
+    if (rxbuf[1] & 0x7) {
+        int16_t val_x = (rxbuf[3] << 8) | rxbuf[2];
+        int16_t val_y = (rxbuf[5] << 8) | rxbuf[4];
+        int16_t val_z = (rxbuf[7] << 8) | rxbuf[6];
+#define MDPS_PER_DIGIT (8.75)
+        *data_x = (((float)val_x) * MDPS_PER_DIGIT)/1000.0;
+        *data_y = (((float)val_y) * MDPS_PER_DIGIT)/1000.0;
+        *data_z = (((float)val_z) * MDPS_PER_DIGIT)/1000.0;
+        return 1;
+    }
+    return 0;
+}
 
 /*
  * Application entry point.
  */
 int main(void) {
-
   /*
    * System initializations.
    * - HAL initialization, this also initializes the configured device drivers
@@ -67,7 +130,7 @@ int main(void) {
    *   RTOS is active.
    */
   halInit();
-  chSysInit();
+  chSysInit(); /* Use Alloc API following line. */
 
   /*
    * Activates the serial driver 1 using the driver default configuration.
@@ -77,10 +140,27 @@ int main(void) {
   palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));
   palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));
 
+  spiStart(&SPID1, &spi1cfg);
+  initGyro();
+
+#if 0
   /*
    * Creates the example thread.
    */
   chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+#endif /* 0 */
+
+  { /* Init Ajhc RTS (Haskell) */
+    int hsargc = 1;
+    char *hsargv = "q";
+    char **hsargvp = &hsargv;
+
+    malloc_init();
+    forkOS_createThread_init();
+
+    hs_init(&hsargc, &hsargvp);
+    _amain();
+  }
 
   /*
    * Normal main() thread activity, in this demo it does nothing except
